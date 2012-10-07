6ab5ba802091a8a5f4bf7358c653d14ad78ecfe7
Kimi <kimi@Kimi-Mint.(none)>
2012-10-07 19:43:24 +0800

    added message model modified user model

---
+++ sns/models/message.py
@@ -0,0 +1,13 @@
+from django.db import models
+from user import User
+
+class Message(models.Model):
+    user = models.ForeignKey(User)
+    post_time = models.DateTimeField('time posted')
+    content = models.CharField(max_length=200)
+    
+    def __unicode__(self):
+        return self.content
+
+    class Meta : 
+        app_label = 'sns'
