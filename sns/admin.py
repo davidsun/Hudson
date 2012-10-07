c5c41d8c9787b48c52bf6f3a6f11863a7dac6de0
Kimi <kimi@Kimi-Mint.(none)>
2012-10-07 19:49:03 +0800

    added admin.py

---
+++ sns/admin.py
@@ -0,0 +1,9 @@
+from sns.models.user import User
+from sns.models.message import Message
+
+from django.contrib import admin
+
+
+admin.site.register(User)
+admin.site.register(Message)
+
--- sns/models/user.py
+++ sns/models/user.py
@@ -1,9 +1,11 @@
 from django.db import models
 
-class User(models.Model) :
-    id = models.BigIntegerField(primary_key = True)
+class User(models.Model):
     name = models.CharField(max_length = 30)
     nickname = models.CharField(max_length = 30)
+    
+    def __unicode__(self):
+        return self.nickname
 
     class Meta : 
         app_label = 'sns'
