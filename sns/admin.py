21de6eb47d6267d1ed2ac446b1cd3df14e56155b
Pursuer <sunzheng.david@gmail.com>
2012-10-14 20:01:25 +0800

    Update admin

--- sns/admin.py
+++ sns/admin.py
@@ -2,5 +2,5 @@ from sns.models import Message, UserProfile
 from django.contrib.auth.models import User
 from django.contrib import admin
 
-admin.site.register(Message)
+admin.site.register(Post)
 admin.site.register(UserProfile)
