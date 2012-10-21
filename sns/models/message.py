89fbd62b1bb2a2c4964f9ca819854a47fe2ca132
alex031029 <alex920101094@163.com>
2012-10-21 21:24:07 +0800

    Update sns/models/message.py

--- sns/models/message.py
+++ sns/models/message.py
@@ -5,8 +5,9 @@ from sns.models.hudson_model import HudsonModel
 class Message(HudsonModel):
     content = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+    receiver = models.ForeignKey(User, related_name='received_messages')
+    sender = models.ForeignKey(User, related_name='sent_messages')
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, related_name='messages')
 
     class Meta : 
         app_label = 'sns'
