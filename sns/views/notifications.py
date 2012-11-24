c96de1305f77c602a50ed456b2fdba6d1d788a32
Pursuer <sunzheng.david@gmail.com>
2012-11-24 15:26:41 +0800

    Update notifications.py to support showing followers & followees.

--- sns/views/notifications.py
+++ sns/views/notifications.py
@@ -3,12 +3,19 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 
+
 @login_required
-def index(request) :
-    followers = list(request.user.followers.all()[:5])
-    followees = list(request.user.followees.all()[:5])
+def index(request):
+    followers = list(User.objects.filter(id__in=list(request.user.followers.values_list('follower_id', flat=True)[:5])))
+    followees = list(User.objects.filter(id__in=list(request.user.followees.values_list('followee_id', flat=True)[:5])))
     latest_users = list(User.objects.order_by('-date_joined')[:5])
+    for follower in followers:
+        follower.followed = follower.followers.filter(follower_id=request.user.id).count() > 0
+    for followee in followees:
+        followee.followed = followee.followers.filter(follower_id=request.user.id).count() > 0
+    for latest_user in latest_users:
+        latest_user.followed = latest_user.followers.filter(follower_id=request.user.id).count() > 0
     notifications = list(request.user.notifications.order_by("-created_at").all())
     request.user.notifications.filter(viewed=False).update(viewed=True)
-    return render_to_response('sns/notifications/index', {'followers':followers, 'followees':followees, 'latest_users':latest_users, 'notifications' : notifications}, context_instance=RequestContext(request))
-
+    return render_to_response('sns/notifications/index', {'followers': followers, 'followees': followees,
+        'latest_users': latest_users, 'notifications': notifications}, context_instance=RequestContext(request))
