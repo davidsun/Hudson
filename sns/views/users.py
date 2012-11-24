1d22e8a0202efeefe012d14c9b810498c4c3d27a
Pursuer <sunzheng.david@gmail.com>
2012-11-24 12:41:15 +0800

    Update follow feature.

--- sns/views/users.py
+++ sns/views/users.py
@@ -14,116 +14,138 @@ from sns.models import Post, UserFollow
 from sns.libs.utils import jsonize, posts_loader
 from sns.views.forms.users import Edit
 
+
 @login_required
 @jsonize
-def follow(request, user_id) :
+def follow(request, user_id):
     user = User.objects.get(id=user_id)
     user.followers.get_or_create(follower_id=request.user.id)
     return {'status': 'ok'}
 
+
 @login_required
 @jsonize
-def followers(request, user_id) :
+def followers(request, user_id):
     followers = list(User.objects.get(id=user_id).followers.all())
     allfollowers = []
     for follower in followers:
-        allfollowers.append({ 'id':follower.follower.id, 'name': follower.follower.username})
+        allfollowers.append({'id': follower.follower.id, 'name': follower.follower.username})
     return allfollowers
+
 
 @login_required
 @jsonize
-def followees(request, user_id) :
+def followees(request, user_id):
     followees = list(User.objects.get(id=user_id).followees.all())
     allfollowees = []
     for followee in followees:
-        allfollowees.append({ 'id':followee.followee.id, 'name': followee.followee.username})
+        allfollowees.append({'id': followee.followee.id, 'name': followee.followee.username})
     return allfollowees
+
 
 @login_required
 @posts_loader('sns/users/index')
-def index(request) :
+def index(request):
     followers = list(User.objects.filter(id__in=list(request.user.followers.values_list('follower_id', flat=True)[:5])))
     followees = list(User.objects.filter(id__in=list(request.user.followees.values_list('followee_id', flat=True)[:5])))
     latest_users = list(User.objects.order_by('-date_joined')[:5])
-    for follower in followers : follower.followed = follower.followers.filter(follower_id=request.user.id).count() > 0
-    for followee in followees : followee.followed = followee.followers.filter(follower_id=request.user.id).count() > 0
-    for latest_user in latest_users : latest_user.followed = latest_user.followers.filter(follower_id=request.user.id).count() > 0
+    for follower in followers:
+        follower.followed = follower.followers.filter(follower_id=request.user.id).count() > 0
+    for followee in followees:
+        followee.followed = followee.followers.filter(follower_id=request.user.id).count() > 0
+    for latest_user in latest_users:
+        latest_user.followed = latest_user.followers.filter(follower_id=request.user.id).count() > 0
     followed_ids = list(request.user.followees.values_list('followee_id', flat=True))
     followed_ids.append(request.user.id)
     posts = Post.objects.filter(user_id__in=followed_ids).order_by("-created_at")
     new_notification_count = request.user.notifications.filter(viewed=False).count()
-    return {'followers':followers, 'followees':followees, 'latest_users':latest_users, 'posts':posts,
-            'new_notification_count':new_notification_count}
+    return {'followers': followers, 'followees': followees, 'latest_users': latest_users, 'posts': posts,
+            'new_notification_count': new_notification_count}
 
-def login(request) :
-    if request.user.is_authenticated() : return redirect('/')
+
+def login(request):
+    if request.user.is_authenticated():
+        return redirect('/')
     from forms.users import Login
-    if request.method == 'POST' :
+    if request.method == 'POST':
         form = Login(request.POST)
-        if form.is_valid() :
+        if form.is_valid():
             form.save(request)
             return HttpResponseRedirect(reverse('sns.views.users.index'))
-        else :
-            return render_to_response('sns/users/login', {'form':form}, context_instance=RequestContext(request))
-    else :
+        else:
+            return render_to_response('sns/users/login', {'form': form}, context_instance=RequestContext(request))
+    else:
         form = Login()
-        return render_to_response('sns/users/login', {'form':form}, context_instance=RequestContext(request))
+        return render_to_response('sns/users/login', {'form': form}, context_instance=RequestContext(request))
+
 
 @login_required
-def logout(request) :
+def logout(request):
     from django.contrib.auth import logout
     logout(request)
     return redirect('/')
+
 
 @login_required
 @posts_loader('sns/users/show')
 def show(request, user_id):
     user = get_object_or_404(User, pk=user_id)
     user.followed = user.followers.filter(follower_id=request.user.id).count() > 0
-    followers = list(user.followers.all()[:5])
-    followees = list(user.followees.all()[:5])
+    followers = list(User.objects.filter(id__in=list(user.followers.values_list('follower_id', flat=True)[:5])))
+    followees = list(User.objects.filter(id__in=list(user.followees.values_list('followee_id', flat=True)[:5])))
+    for follower in followers:
+        follower.followed = follower.followers.filter(follower_id=request.user.id).count() > 0
+    for followee in followees:
+        followee.followed = followee.followers.filter(follower_id=request.user.id).count() > 0
     posts = Post.objects.filter(user=user).order_by("-created_at")
-    return {'followers':followers, 'followees':followees, 'posts':posts, 'user':user}
+    return {'followers': followers, 'followees': followees, 'posts': posts, 'user': user}
+
 
 @login_required
-def search(request) :
+def search(request):
     users = list(User.objects.filter(username__icontains=request.GET.get('q', '')).all())
-    for user in users : user.followed = user.followers.filter(follower_id=request.user.id).count() > 0
-    return render_to_response('sns/users/search', {'users':users}, context_instance=RequestContext(request)) 
+    for user in users:
+        user.followed = user.followers.filter(follower_id=request.user.id).count() > 0
+    return render_to_response('sns/users/search', {'users': users}, context_instance=RequestContext(request))
 
-def signup(request) :
-    if request.user.is_authenticated() : return redirect('/')
+
+def signup(request):
+    if request.user.is_authenticated():
+        return redirect('/')
     from forms.users import Signup
-    if request.method == 'POST' :
+    if request.method == 'POST':
         form = Signup(request.POST)
-        if form.is_valid() :
+        if form.is_valid():
             form.save()
             return redirect('/')
-        else :
-            return render_to_response('sns/users/signup', {'form':form}, context_instance=RequestContext(request))
-    else :
+        else:
+            return render_to_response('sns/users/signup', {'form': form}, context_instance=RequestContext(request))
+    else:
         form = Signup()
-        return render_to_response('sns/users/signup', {'form':form}, context_instance=RequestContext(request))
+        return render_to_response('sns/users/signup', {'form': form}, context_instance=RequestContext(request))
+
 
 @login_required
 @jsonize
-def unfollow(request, user_id) :
+def unfollow(request, user_id):
     UserFollow.objects.filter(follower_id=request.user.id, followee_id=user_id).delete()
     return {'status': 'ok'}
 
+
 @login_required
-def edit(request, user_id) :
-    if int(user_id) != request.user.id : return redirect('/')
+def edit(request, user_id):
+    if int(user_id) != request.user.id:
+        return redirect('/')
     if request.method == 'POST':
         form = Edit(user=request.user, data=request.POST)
         if form.is_valid():
             form.save()
             return redirect('/')
         else:
-            return render_to_response('sns/users/edit', {'form':form}, context_instance=RequestContext(request))
+            return render_to_response('sns/users/edit', {'form': form}, context_instance=RequestContext(request))
     else:
         form = Edit()
-        return render_to_response('sns/users/edit', {'form':form}, context_instance=RequestContext(request))
+        return render_to_response('sns/users/edit', {'form': form}, context_instance=RequestContext(request))
 
 
 @login_required
@@ -150,7 +172,8 @@ def contact(request, query):
     else:
         others.extend(contacts)
         other_ids.extend(contact_ids)
-    chated_ids = list(request.user.sent_messages.all().filter(sender_id__in=other_ids).order_by('-created_at').values_list('receiver_id', flat=True)[:count])
+    chated_ids = list(request.user.sent_messages.all().filter(sender_id__in=other_ids).order_by('-created_at')
+        .values_list('receiver_id', flat=True)[:count])
     chated = User.objects.filter(id__in=chated_ids)
     total = []
     total.extend(prefix[:count])
---
+++ static/js/users.js
@@ -0,0 +1,38 @@
+$.users = {
+  showFollowButton: function(element){
+    var t = $(element);
+    var user_id = t.attr("user-id");
+    if (t.attr("followed") == "true"){
+      t.removeClass("btn-danger").removeClass("btn-primary").html("已关注");
+      t.unbind('hover').hover(function(){
+        t.addClass("btn-danger").html("取消关注");
+      }, function(){
+        t.removeClass("btn-danger").html("已关注");
+      });
+    } else {
+      t.removeClass("btn-danger").addClass("btn-primary").html("关注");
+      t.unbind("hover");
+    }
+    t.unbind('click').click(function(){
+      if (t.attr("followed") == "true"){
+        $.get("/users/" + user_id + "/unfollow");
+        t.attr("followed", "false");
+      } else {
+        $.get("/users/" + user_id + "/follow");
+        t.attr("followed", "true");
+      }
+      $.users.showFollowButton(t);
+    });
+  },
+
+  showInSidebarList: function(element){
+    var t = $(element);
+    t.find("a[data-toggle='follow-button']").each(function(){
+      $.users.showFollowButton(this);
+    });
+  }
+};
+
+$.fn.showSidebarListUser = function(){
+  $.users.showInSidebarList(this);
+};
