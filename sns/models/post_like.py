63705074215ab7c259a6eb2348495b919973b8f2
Pursuer <sunzheng.david@gmail.com>
2012-12-01 10:36:00 +0800

    Update PostLike model.

--- sns/models/post_like.py
+++ sns/models/post_like.py
@@ -1,20 +1,22 @@
-# -*- coding: utf8 -*- 
+# -*- coding: utf8 -*-
 
 from django.contrib.auth.models import User
 from django.core import validators
-from django.db import models 
+from django.db import models
 from sns.models.hudson_model import HudsonModel
 from sns.models.post import Post
 
-class PostLike(HudsonModel) :
+
+class PostLike(HudsonModel):
     created_at = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey(Post, related_name='likes')
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, related_name='post_likes')
 
-    class Meta :
+    class Meta:
         app_label = 'sns'
-    
-    def clean(self) :
+
+    def clean(self):
         objs = PostLike.objects.filter(user_id=self.user_id, post_id=self.post_id).all()
-        if len(objs) > 0 and objs[0].id != self.id : raise validators.ValidationError(u'不能重复收藏。')
+        if len(objs) > 0 and objs[0].id != self.id:
+            raise validators.ValidationError(u'不能重复收藏。')
